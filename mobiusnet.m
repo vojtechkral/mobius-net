2bbbf73a4bde6a710cda634b5f1eb6e7d914cda2
Vojtech Kral <vojtech@kral.hk>
2014-12-02 21:18:17 +0100

    Move callbacks into main function

--- mobiusnet.m
+++ mobiusnet.m
@@ -12,19 +12,22 @@ function mobiusnet()
 			'Title', 'Input settings', 'BackgroundColor', 'none'); % bgcolor Screws title
 
 	gui.tbl = uitable(gui.settings, 'Units', 'normalized',...
-			'Position', [0 .75 1 .25], 'Data', zeros(5, 2),...
+			'Position', [0 .7 1 .3], 'Data', zeros(5, 2),...
 			'ColumnName', {'X', 'Y'},...
 			'RowName', {'O', 'A' 'B', 'Sa', 'Sb'},...
 	'ColumnWidth', {100, 200});    
 	gui.pick = uicontrol(gui.settings, 'Style', 'pushbutton',...
-			'Units', 'normalized', 'Position', [.1 .7 .8 .04],...
+			'Units', 'normalized', 'Position', [.1 .65 .8 .04],...
 			'String', 'Pick coordinates');
 	gui.render = uicontrol(gui.settings, 'Style', 'pushbutton',...
-			'Units', 'normalized', 'Position', [.1 .65 .8 .04],...
+			'Units', 'normalized', 'Position', [.1 .60 .8 .04],...
 			'String', 'Render Möbius Net');
 	gui.save = uicontrol(gui.settings, 'Style', 'pushbutton',...
-			'Units', 'normalized', 'Position', [.1 .60 .8 .04],...
+			'Units', 'normalized', 'Position', [.1 .55 .8 .04],...
 			'String', 'Save image');
+	gui.save = uicontrol(gui.settings, 'Style', 'pushbutton',...
+		'Units', 'normalized', 'Position', [.1 .50 .8 .04],...
+		'String', 'Reset');
 		
 	% TODO: reset btn
 
@@ -35,28 +38,56 @@ function mobiusnet()
 	gui.axesSz = getpixelposition(gui.axes);
 	gui.axesSz = gui.axesSz(3:4);
 
-	%% Callbacks (bind _after_ gui components are created)
-	set(gui.pick, 'Callback', @(hO, evt)pickCoords(gui));
-	set(gui.settings, 'ResizeFcn', @(hO, evt)settingsResize(gui));
+	%% Callbacks  % TODO: move into code above
+	set(gui.pick, 'Callback', @pickCoords);
+	set(gui.settings, 'ResizeFcn', @settingsResize);
 
 	%% Reset & wait for input
-		resetAll(gui);
+		resetAll();
+		
+	%% Callback functions
+	function pickCoords(hO, evt)
+		resetAll();
+		O = getPoint('O', gui);
+		A = getPoint('A', gui);
+		plotLine(O, A);
+		B = getPoint('B', gui);
+		plotLine(O, B);
+		plotLine(A, B);
+
+		Sa = getPoint('Sa', gui, O, A);
+		plotLine(Sa, B);
+		Sb = getPoint('Sb', gui, O, B);
+		plotLine(Sb, A);
+
+		data = [O; A; B; Sa; Sb];
+		set(gui.tbl, 'Data', data);
+	end
+
+	function settingsResize(hO, evt)
+		width = getpixelposition(gui.settings);
+		width = width(3)/2 - 20;
+		set(gui.tbl, 'ColumnWidth', {width, width});
+	end
+
+	%% Resets all states to initial
+	function resetAll()
+			defaultData = [
+					0 0
+					0 1;
+					1 0;
+					0 0.1;
+					0.1 0
+				];
+			cla(gui.axes);
+			fill([0 1 1 0], [0 0 1 1], 'white');
+			set(gui.tbl, 'Data', defaultData);
+	end
+
 end
 
-%% Resets all states to initial
-function resetAll(gui)
-		defaultData = [
-				0 0
-				0 1;
-				1 0;
-				0 0.1;
-				0.1 0
-			];
-		cla(gui.axes);
-		fill([0 1 1 0], [0 0 1 1], 'white');
-		set(gui.tbl, 'Data', defaultData);
-end
 
+%% Utilities
 function ret = projectOnLine(p, A, B)
 	AB = (B-A);
 	dist = dot(AB,AB);
@@ -80,6 +111,7 @@ function point = getPoint(label, gui, lA, lB)
 
 	if nargin == 4
 		point = projectOnLine(point, lA, lB);
+		% FIXME: if point is outside line, set default?
 	end
 	
 	w = gui.axesSz(1);
@@ -99,29 +131,6 @@ end
 
 function plotLine(A, B)
 	AB = [A;B];
-	line(AB(:,1), AB(:,2), 'Color', 'green');
+	line(AB(:,1), AB(:,2));
 end
 
-function pickCoords(gui)
-%     resetAll();    % FIXME:
-	O = getPoint('O', gui);
-	A = getPoint('A', gui);
-	plotLine(O, A);
-	B = getPoint('B', gui);
-	plotLine(O, B);
-	plotLine(A, B);
-	
-	Sa = getPoint('Sa', gui, O, A);
-	plotLine(Sa, B);
-	Sb = getPoint('Sb', gui, O, B);
-	plotLine(Sb, A);
-	
-	data = zeros(2, 5);
-%     get(gui.tbl, 'Data');
-end
-
-function settingsResize(gui)
-	width = getpixelposition(gui.settings);
-	width = width(3)/2 - 20;
-	set(gui.tbl, 'ColumnWidth', {width, width});
-end
