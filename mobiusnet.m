873a061fad6d266329507d52d97a1dab20145388
Vojtech Kral <vojtech@kral.hk>
2014-12-16 14:06:28 +0100

    Mobius Net rendering algorithm

--- mobiusnet.m
+++ mobiusnet.m
@@ -1,7 +1,8 @@
+% FIXME: status
 
 function mobiusnet()
 	% mobiusnet - Renders a Mobius Net
-	% TODO: doc
+	% FIXME: doc
 
 	%% setup GUI
 	gui = {};
@@ -15,14 +16,14 @@ function mobiusnet()
 	gui.tbl = uitable(gui.settings, 'Units', 'normalized',...
 		'Position', [0 .7 1 .3], 'Data', zeros(5, 2),...
 		'ColumnName', {'X', 'Y'},...
-		'RowName', {'O', 'A' 'B', 'Sa', 'Sb'},...
+		'RowName', {'O', 'X' 'Y', 'x₁', 'y₁'},...
 		'ColumnWidth', {100, 200});    
 	gui.pick = uicontrol(gui.settings, 'Style', 'pushbutton',...
 		'Units', 'normalized', 'Position', [.1 .65 .8 .04],...
 		'String', 'Pick coordinates', 'Callback', @pickCoords);
 	gui.render = uicontrol(gui.settings, 'Style', 'pushbutton',...
 		'Units', 'normalized', 'Position', [.1 .60 .8 .04],...
-		'String', 'Render Möbius Net');
+		'String', 'Render Möbius Net', 'Callback',@render);
 	gui.save = uicontrol(gui.settings, 'Style', 'pushbutton',...
 		'Units', 'normalized', 'Position', [.1 .55 .8 .04],...
 		'String', 'Save image');
@@ -44,20 +45,22 @@ function mobiusnet()
 	
 	%% Event callback functions
 	function pickCoords(hO, evt)
+		% FIXME: inform which point is being picked 
+		%	       (label / status bar / row color)
 		resetAll();
 		O = getPoint('O', gui);
-		A = getPoint('A', gui);
-		plotLine(O, A);
-		B = getPoint('B', gui);
-		plotLine(O, B);
-		plotLine(A, B);
+		X = getPoint('X', gui);
+		plotLine(O, X);
+		Y = getPoint('Y', gui);
+		plotLine(O, Y);
+		plotLine(X, Y);
 
-		Sa = getPoint('Sa', gui, O, A);
-		plotLine(Sa, B);
-		Sb = getPoint('Sb', gui, O, B);
-		plotLine(Sb, A);
+		x1 = getPoint('x1', gui, O, X);
+		plotLine(x1, Y);
+		y1 = getPoint('y1', gui, O, Y);
+		plotLine(y1, X);
 
-		data = [O; A; B; Sa; Sb];
+		data = [O; X; Y; x1; y1];
 		set(gui.tbl, 'Data', data);
 	end
 
@@ -67,13 +70,18 @@ function mobiusnet()
 		set(gui.tbl, 'ColumnWidth', {width, width});
 	end
 
+	function render(h0, evt)
+		renderMobiusNet(gui);
+	end
+
 	function resetAll(hO, evt)
+		% FIXME: cancel ginput if any
 		defaultData = [
 				0 0
-				0 1;
 				1 0;
-				0 0.1;
-				0.1 0
+				0 1;
+				0.1 0;
+				0 0.1
 			];
 		cla(gui.axes);
 		fill([0 1 1 0], [0 0 1 1], 'white');
@@ -130,3 +138,102 @@ function plotLine(A, B)
 	line(AB(:,1), AB(:,2));
 end
 
+function fillQuad(points, color)
+	% shorthand for easier quadrilateral polygon fill
+	fill(points(:,1), points(:,2), color);
+end
+
+function point = lineIntersect(a1, a2, b1, b2)
+	l1 = cross([a1 1], [a2 1]);
+	l2 = cross([b1 1], [b2 1]);
+	point = cross(l1, l2);
+	point = point(1:2) / point(3);
+end
+
+%% Mobius net algorithm
+function renderMobiusNet(gui)
+		% Clear axes:
+		cla(gui.axes);
+		fill([0 1 1 0], [0 0 1 1], 'white');
+
+		% Setup rendering context:
+		ctx = {};
+		data = get(gui.tbl, 'Data');
+		ctx.O = data(1,:);
+		ctx.X = data(2,:);
+		ctx.Y = data(3,:);
+		ctx.x1 = data(4,:);
+		ctx.y1 = data(5,:);
+		
+		% Compute first internal (p11) and horizon (H) points:
+		p11 = lineIntersect(ctx.x1, ctx.Y, ctx.y1, ctx.X);
+		ctx.H = lineIntersect(ctx.O, p11, ctx.X, ctx.Y);  
+		
+		% Buffer for internals points:
+		ctx.ps = [ctx.y1; p11; ctx.x1];
+		
+		% Buffers for x and y points:
+		ctx.xs = [ctx.x1];
+		ctx.ys = [ctx.y1];
+		
+		% Checkerboard colors:
+		ctx.colors = [[0 0 0]; [1 1 1]];
+		ctx.run = 0;  % indicates current run: odd or even
+		
+		% Draw the first quadrilateral:
+		fillQuad([ctx.O; ctx.x1; p11; ctx.y1], ctx.colors(1,:));
+		
+		% Draw checkerboard waves until distance between them
+		% becomes insignificant
+		threshold = 1e-3;
+		ctx.dist = Inf;     % distance between last and new wave
+		while ctx.dist > threshold
+			ctx = mobiusWave(ctx);
+		end
+end
+
+function ctx = mobiusWave(ctx)
+	% Compute intermediate points:
+	px = lineIntersect(ctx.xs(end,:), ctx.H, ctx.y1, ctx.X);
+	py = lineIntersect(ctx.ys(end,:), ctx.H, ctx.x1, ctx.Y);
+
+	% Compute this wave's x & y:
+	xn = lineIntersect(px, ctx.Y, ctx.O, ctx.X);
+	yn = lineIntersect(py, ctx.X, ctx.O, ctx.Y);
+	
+	% Generate list of this wave's points:
+	mid = ceil(size(ctx.ps, 1) / 2); % Idx of the middle point in previsou ps
+	ps = [yn; py];
+	for ix = 2:mid
+		isec = lineIntersect(ctx.ps(ix,:), ctx.H, yn, ctx.X);
+		ps = [ps; isec];
+	end
+	for iy = mid+1:size(ctx.ps, 1)-1
+		isec = lineIntersect(ctx.ps(iy,:), ctx.H, xn, ctx.Y);
+		ps = [ps; isec];
+	end
+	ps = [ps; px; xn];
+
+	% Draw quadrilaterals:
+	%  triplicate the middle points in previous ps
+	%  for easier polygon drawing:
+	pprev = [ ctx.ps(1:mid,:); ctx.ps(mid,:); ctx.ps(mid:end,:) ];
+	mid = mid + 1;
+	%  generate a color pattern for this wave:
+	pattern = mod([1:mid-1 mid-1:-1:1] + ctx.run, 2) + 1; 
+	for ip = 1:size(ps, 1)-1
+		fillQuad([pprev([ip ip+1],:); ps([ip+1 ip],:)], ...
+		         ctx.colors(pattern(ip),:));
+	end
+
+	% Compute this wave's distance:
+	xdist = norm(xn - ctx.xs(end,:));
+	ydist = norm(yn - ctx.ys(end,:));
+	ctx.dist = max([xdist ydist]);
+	
+	% Update context data for next round:
+	ctx.ps = ps;
+	ctx.xs = [ctx.xs; xn];
+	ctx.ys = [ctx.ys; yn];
+	ctx.run = ~ctx.run;
+end
